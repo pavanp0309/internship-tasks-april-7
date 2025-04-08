939365233153fd9476a4144f5ae908173e579d3c
VivekEndla <endlavivek81@gmail.com>
2025-04-08 18:34:08 +0530

    Block-completed-8-4-25

---
+++ vivek/Blog/src/components/Card.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='end'>
+        <h1 className='heading'>Blogs on Cars</h1>
+        <div className='main'>
+        <div className='card'>
+            <img className='card-img' src="./src/assets/cupra.webp" alt="" />
+            <h4 className='card-title'>Cupra</h4>
+            <p className='card-desc'>The Cupra Formentor is up to an all-time high 5th place in Germany in March. 253,497 new cars hit German roads in March, a -3.7% year-on-year fall. The Q1 volume is off -4.3% to 664,571. 42,521 BEVs found a buyer this month, up 35.5% over March 2024.  Over Q1, it's…</p>
+            <div className='info'>
+            <h5 className='author'><img className='user' src="./src/assets/clock.png" alt="" /> April 8, 2025</h5>
+            <h5 className='author'><img className='user' src="./src/assets/user.png" alt="" /> Matt Gasiner</h5>
+            </div>
+            <button className='card-btn'>Readmore</button>
+        </div>
+        <div className='card'>
+            <img className='card-img' src="./src/assets/Toyota-RAV4.webp" alt="" />
+            <h4 className='card-title'>ToyotaRAV4</h4>
+            <p className='card-desc'>The Toyota RAV4 is the best-selling vehicle in Finland in March. Finnish new car sales endure a third straight negative month in March (and 15th out of the past 16 months) at -4.7% to 6,082 units. The Q1 tally is off -9.4% to 16,447. Toyota (-21.4%) keeps the brands pole…</p>
+            <div className='info'>
+            <h5 className='author'><img className='user' src="./src/assets/clock.png" alt="" /> April 9, 2025</h5>
+            <h5 className='author'><img className='user' src="./src/assets/user.png" alt="" /> Jeff Mandle</h5>
+            </div>
+            <button className='card-btn'>Readmore</button>
+        </div>
+        <div className='card'>
+            <img className='card-img' src="./src/assets/Tesla-Model-Y.webp" alt="" />
+            <h4 className='card-title'>TeslaModelY</h4>
+            <p className='card-desc'>The Tesla Model Y is the best-selling vehicle in Norway in March. The Norwegian new car market rallies back up 36.5% year-on-year in March to 13,304 units, keeping in mind March 2024 was down -49.7% on the year prior. The Q1 volume is up 42% to 31,596. 11,192 BEVs were…</p>
+            <div className='info'>
+            <h5 className='author'><img className='user' src="./src/assets/clock.png" alt="" /> April 8, 2025</h5>
+            <h5 className='author'><img className='user' src="./src/assets/user.png" alt="" /> Matt Gasiner</h5>
+            </div>
+            <button className='card-btn'>Readmore</button>
+        </div>
+        <div className='card'>
+            <img className='card-img' src="./src/assets/Hyundai-creta.webp" alt="" />
+            <h4 className='card-title'>Hyundaicreta</h4>
+            <p className='card-desc'>The Solaris HC is a Hyundai Creta. The Russian new car market continues to freefall in March at a daunting -45.5% year-on-year to just 79,822 units. The Q1 tally is off -25.3% to 246,934. Lada (-36.9%) falls slower than its home market and improves its share to 31.5%. Haval (-39%)…</p>
+            <div className='info'>
+            <h5 className='author'><img className='user' src="./src/assets/clock.png" alt="" /> April 8, 2025</h5>
+            <h5 className='author'><img className='user' src="./src/assets/user.png" alt="" /> Matt Gasiner</h5>
+            </div>
+            <button className='card-btn'>Readmore</button>
+        </div>
+        </div>
+        <div className='main'>
+        <div className='card'>
+            <img className='card-img' src="./src/assets/cupra.webp" alt="" />
+            <h4 className='card-title'>Cupra</h4>
+            <p className='card-desc'>The Cupra Formentor is up to an all-time high 5th place in Germany in March. 253,497 new cars hit German roads in March, a -3.7% year-on-year fall. The Q1 volume is off -4.3% to 664,571. 42,521 BEVs found a buyer this month, up 35.5% over March 2024.  Over Q1, it's…</p>
+            <div className='info'>
+            <h5 className='author'><img className='user' src="./src/assets/clock.png" alt="" /> April 8, 2025</h5>
+            <h5 className='author'><img className='user' src="./src/assets/user.png" alt="" /> Matt Gasiner</h5>
+            </div>
+            <button className='card-btn'>Readmore</button>
+        </div>
+        <div className='card'>
+            <img className='card-img' src="./src/assets/Toyota-RAV4.webp" alt="" />
+            <h4 className='card-title'>ToyotaRAV4</h4>
+            <p className='card-desc'>The Toyota RAV4 is the best-selling vehicle in Finland in March. Finnish new car sales endure a third straight negative month in March (and 15th out of the past 16 months) at -4.7% to 6,082 units. The Q1 tally is off -9.4% to 16,447. Toyota (-21.4%) keeps the brands pole…</p>
+            <div className='info'>
+            <h5 className='author'><img className='user' src="./src/assets/clock.png" alt="" /> April 9, 2025</h5>
+            <h5 className='author'><img className='user' src="./src/assets/user.png" alt="" /> Jeff Mandle</h5>
+            </div>
+            <button className='card-btn'>Readmore</button>
+        </div>
+        <div className='card'>
+            <img className='card-img' src="./src/assets/Tesla-Model-Y.webp" alt="" />
+            <h4 className='card-title'>TeslaModelY</h4>
+            <p className='card-desc'>The Tesla Model Y is the best-selling vehicle in Norway in March. The Norwegian new car market rallies back up 36.5% year-on-year in March to 13,304 units, keeping in mind March 2024 was down -49.7% on the year prior. The Q1 volume is up 42% to 31,596. 11,192 BEVs were…</p>
+            <div className='info'>
+            <h5 className='author'><img className='user' src="./src/assets/clock.png" alt="" /> April 8, 2025</h5>
+            <h5 className='author'><img className='user' src="./src/assets/user.png" alt="" /> Matt Gasiner</h5>
+            </div>
+            <button className='card-btn'>Readmore</button>
+        </div>
+        <div className='card'>
+            <img className='card-img' src="./src/assets/Hyundai-creta.webp" alt="" />
+            <h4 className='card-title'>Hyundaicreta</h4>
+            <p className='card-desc'>The Solaris HC is a Hyundai Creta. The Russian new car market continues to freefall in March at a daunting -45.5% year-on-year to just 79,822 units. The Q1 tally is off -25.3% to 246,934. Lada (-36.9%) falls slower than its home market and improves its share to 31.5%. Haval (-39%)…</p>
+            <div className='info'>
+            <h5 className='author'><img className='user' src="./src/assets/clock.png" alt="" /> April 8, 2025</h5>
+            <h5 className='author'><img className='user' src="./src/assets/user.png" alt="" /> Matt Gasiner</h5>
+            </div>
+            <button className='card-btn'>Readmore</button>
+        </div>
+        </div>
+    </div>
+    
+
+    
+    
+  )
+}
+
+export default Card
