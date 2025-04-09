a446d2ab872419e48bad51f89abb8a812fb850ab
sowmyasomisetty <snsowmya2001@gmail.com>
2025-04-10 01:12:15 +0530

    food-blopage 10/04/2025

---
+++ sowmya/blog-page/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BlogList from "./components/BlogList";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="main-heading">Delicious Food Blog</h1>
+      <BlogList />
+    </div>
+  );
+};
+
+export default App;
---
+++ sowmya/blog-page/src/data/blogData.js
@@ -0,0 +1,26 @@
+const blog1 = {
+    title: "Top 5 Street Foods You Must Try",
+    description: "From spicy chaats to sizzling skewers, explore the best street food from around the world.",
+    author: "Suneel Kumar",
+    image: "src/assets/street food.jpg",
+    tags: ["#StreetFood", "#Spicy"],
+  };
+  
+  const blog2 = {
+    title: "Healthy Breakfast Ideas",
+    description: "Kickstart your day with these delicious and nutritious breakfast options. Whether you're in a rush or have time to cook.",
+    author: "Sowmya somisetty",
+    image: "src/assets/healthy breakfast.jpg",
+    tags: ["#Breakfast", "#HealthyEating"],
+  };
+  
+  const blog3 = {
+    title: "Exploring Global Desserts",
+    description: "Satisfy your sweet tooth with unique desserts from different cultures. This post will take your tastebuds on a sugary journey around the world!",
+    author: "Kavya Sudha",
+    image: "src/assets/street food.jpg",
+    tags: ["#Dessert", "#Foodie"],
+  };
+  
+  export { blog1, blog2, blog3 };
+  
