8fbf54f8901e598cea2f6873015218dbe36fbc08
GOPUBHAVANI <nigabhavi@132gmail.com>
2025-04-09 15:10:15 +0530

    landpage completed-09-04-2025 3:10

---
+++ Bhavani/project_2_landing_page/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Features from './pages/Features'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar/>
+      <Features/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
