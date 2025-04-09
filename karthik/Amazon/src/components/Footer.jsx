56870b3866c909822337066c42fccd40220b92bb
BijjaKarthik <bkarthik20012002@gmail.com>
2025-04-09 15:54:45 +0530

    amazon 8-4-25

---
+++ karthik/Amazon/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+
+const Footer = () => (
+    <footer className="bg-dark text-white text-center p-3 mt-4">
+      <p>&copy; 2025 Amazon UI Clone. All rights reserved.</p>
+    </footer>
+  );
+  
+  export default Footer;
+  
---
+++ karthik/Amazon/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+const Navbar = () => (
+    <nav className="navbar navbar-dark bg-dark px-3">
+      <span className="navbar-brand mb-0 h1">Amazon Clone</span>
+    </nav>
+  );
+  
+  export default Navbar;
+  
