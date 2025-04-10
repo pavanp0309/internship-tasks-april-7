e544c56c8cc51397627910c0500c78faa75470b3
SureshMandal <sureshmandal7989@gmail.com>
2025-04-10 18:07:17 +0530

    Daily Standup Board Completed 10/04/2025 06:07pm

---
+++ Suresh/Daily Standup Board/Daily Standup Board/src/components/Marketing.js
@@ -0,0 +1,17 @@
+export let marketing = [
+    {
+        name:"Shakti",
+        Yesterday: "Assigned to complete the template",
+        Today: "Assigned to complete the template",
+        Blocker: ""
+    },
+    {name:"Dhruv",
+        Yesterday: "Assigned to complete the template",
+        Today: "Assigned to complete the template",
+        Blocker: ""},
+    {name:"Pankaj",
+        Yesterday: "Assigned to complete the template",
+        Today: "Assigned to complete the template",
+        Blocker: ""},
+    ]
+    
