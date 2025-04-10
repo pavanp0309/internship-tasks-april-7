961b12bcadfc00ed1eebe1fe64d7c3158df5f922
sumithreddy <sumithreddymukku@gmail.com>
2025-04-10 19:20:29 +0530

    'landingpage-10-04-2025-7:18PM'

---
+++ sumith/LandingPage/LandingPage/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Features from './pages/Features'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Features/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default App
