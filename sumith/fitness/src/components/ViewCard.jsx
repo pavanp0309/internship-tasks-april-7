f6f319c695e4a2a1d0fd5273f2bf673e89996c75
sumithreddy <sumithreddymukku@gmail.com>
2025-04-13 17:50:16 +0530

    fitness completed-13-04-2025 5:50PM

---
+++ sumith/fitness/src/components/ViewCard.jsx
@@ -0,0 +1,66 @@
+import React from 'react' 
+
+const ViewCard = () => {
+
+  return (
+    <>
+    <div className='container'>
+    <div className='text-center'>  
+    <h5>_____OUR TEAM______</h5>
+    <h2>TRAIN WITH EXPERTS</h2>
+    </div>
+
+    <div className='middle'> 
+    <div className="row row-cols-1 row-cols-md-3 g-4">        
+    <div class="card p-0  " >
+  <img src="./src/assets/img2.webp" class="card-img-top" alt="" />
+  <div class="card-body">
+    <h5 class="card-title">Gym Trainer</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content._</p>
+  </div>
+</div>
+<div class="card p-0 " >
+  <img src="./src/assets/img3.jpeg" class="card-img-top" alt=""/>
+  <div class="card-body">
+    <h5 class="card-title">Gym Trainer</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card p-0" >
+  <img src="./src/assets/img4.jpg" class="card-img-top" alt=""/>
+  <div class="card-body">
+    <h5 class="card-title">Gym Trainer</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+<div className="row row-cols-1 row-cols-md-3 g-4 my-5">
+<div class="card p-0" >
+  <img src="./src/assets/img5.jpeg" class="card-img-top" alt=""/>
+  <div class="card-body">
+    <h5 class="card-title">Gym Trainer</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card p-0" >
+  <img src="./src/assets/img6.jpeg" class="card-img-top" alt=""/>
+  <div class="card-body">
+    <h5 class="card-title">Gym Trainer</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card p-0" >
+  <img src="./src/assets/img7.jpg" class="card-img-top" alt=""/>
+  <div class="card-body">
+    <h5 class="card-title">Gym Trainer</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+</div>
+    </div>
+    </>
+  )
+}
+
+export default ViewCard
