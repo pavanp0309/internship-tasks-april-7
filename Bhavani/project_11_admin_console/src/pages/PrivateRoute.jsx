24cf860acae366e2cbf79159923a3ce8be4d9e5b
GOPUBHAVANI <nigabhavi@132gmail.com>
2025-04-17 11:23:39 +0530

    admin completed 17-04-2025 11:23

---
+++ Bhavani/project_11_admin_console/src/pages/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { getAuthStatus } from '../auth';  // Import the function correctly
+
+const PrivateRoute = () => {
+  const isAuthenticated = getAuthStatus();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  // Render the children (nested routes)
+  return <Outlet />;
+};
+
+export default PrivateRoute;
