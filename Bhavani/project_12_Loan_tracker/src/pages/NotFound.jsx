97d47e03a78c5e718b4649d438dadffe477a5eba
GOPUBHAVANI <nigabhavi@132gmail.com>
2025-04-17 12:15:41 +0530

    loan completed 17-04-2025 12:15

---
+++ Bhavani/project_12_Loan_tracker/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div className=' text-center p-4 m-4 text-white'>
+      <h3>404 - Page Not Found</h3>
+      <p>The page you're looking for does not exist.</p>
+    </div>
+  );
+}
+
+export default NotFound;
